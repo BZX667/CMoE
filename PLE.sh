b129fa39719add848588434788a21f69aeaed92c
BZX667 <87808897+BZX667@users.noreply.github.com>
2023-10-08 18:38:40 +0800

    Add files via upload

---
+++ PLE.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+source /etc/profile
+set -e
+# here is actually yesterday
+today=$(date "+%Y%m%d" -d "-1day")
+/root/miniconda3/envs/py368_tf115/bin/python3.6 /git/alg_rep/MODELS/HUDONG/CVR/PLE.py \
+--task_type=train \
+--learning_rate=0.0006 \
+--optimizer=Adam \
+--num_epochs=1 \
+--batch_size=10240 \
+--field_size=75 \
+--feature_size=1500000 \
+--deep_layers=512,256,128 \
+--dropout=0.9,0.9,0.9 \
+--log_steps=30 \
+--num_threads=12 \
+--clear_existing_model=True
+
+/root/miniconda3/envs/py368_tf115/bin/python3.6 /git/alg_rep/MODELS/HUDONG/CVR/PLE.py \
+--task_type=eval \
+--learning_rate=0.0006 \
+--optimizer=Adam \
+--num_epochs=1 \
+--batch_size=10240 \
+--field_size=75 \
+--feature_size=1500000 \
+--deep_layers=512,256,128 \
+--dropout=0.9,0.9,0.9 \
+--log_steps=30 \
+--num_threads=12 \
+
+#servable_model_dir=$"/data/servable_model/""servable_model_"$today"_ple"
+#/root/miniconda3/envs/new_env/bin/python3.6 /git/alg_rep/MODELS/HUDONG/CVR/PLE.py \
+#--task_type=export \
+#--learning_rate=0.0006 \
+#--optimizer=Adam \
+#--num_epochs=1 \
+#--batch_size=10240 \
+#--field_size=75 \
+#--feature_size=1500000 \
+#--deep_layers=512,256,128 \
+#--dropout=0.9,0.9,0.9 \
+#--log_steps=30 \
+#--num_threads=12 \
+#--servable_model_dir=$servable_model_dir
