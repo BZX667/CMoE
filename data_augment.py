11ebfb99a98737b236a7fa8d60aca7c6aad1c938
BZX667 <87808897+BZX667@users.noreply.github.com>
2023-10-19 11:55:58 +0800

    Update data_augment.py

--- data_augment.py
+++ data_augment.py
@@ -29,9 +29,7 @@ def add_gaussian_noise(x_concat, std=0.01):
 
 def add_uniform_noise(x_concat, weight):
     random_noise = tf.random.uniform(shape=tf.shape(x_concat))
-    # 生成与x_concat相同size的噪声, 填充均匀分布的随机数值
     x_concat += tf.sign(x_concat) * tf.nn.l2_normalize(random_noise, axis=-1) * weight
-    # 控制噪声的两个条件，并生成新的view
     return x_concat
 
 
@@ -50,15 +48,11 @@ def cfm_masking_F_L(x, mask_prob, labels):
         load_mi_values(x, labels)
         sorted_indices = np.argsort(mi_values)
         num_masked = round(mask_prob * x.shape[1])
-        # 比较低的互信息indices
         mask_indices = sorted_indices[:num_masked]
         is_first_batch = False
-    # 比较高的互信息indices
     # mask_indices = sorted_indices[-num_masked:]
-    # 将对应的特征列置为0
     x_masked = np.copy(x)
     x_masked[:, mask_indices] = 0.0
-    # 将NumPy数组转换为TensorFlow张量
     x_masked_tensor = tf.convert_to_tensor(x_masked, dtype=tf.float32)
     return x_masked_tensor
 
@@ -77,8 +71,8 @@ def load_mi_values(x, labels):
 def compute_similarity_matrix(x):
     num_features = x.shape[1]
     similarity_matrix = np.dot(x.T, x) / np.outer(np.linalg.norm(x, axis=0), np.linalg.norm(x, axis=0))
-    similarity_matrix[np.isnan(similarity_matrix)] = 0  # 处理由于除以0导致的NaN值
-    similarity_matrix[np.isinf(similarity_matrix)] = 0  # 处理由于除以0导致的Inf值
+    similarity_matrix[np.isnan(similarity_matrix)] = 0
+    similarity_matrix[np.isinf(similarity_matrix)] = 0
     similarity_matrix = np.triu(similarity_matrix) + np.triu(similarity_matrix, 1).T  # 计算上三角部分和对称部分
     assert similarity_matrix.shape == (num_features, num_features)
     return similarity_matrix
@@ -98,28 +92,23 @@ def load_mi_matrix(x):
 
 def cfm_masking_F_F(x, mask_prob, num_features):
     global is_first_batch, mi_matrix
-    # sees run x to numoy array
     init_op = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init_op)
         x = sess.run(x)
-    # only load mi_matrix in first_batch and global it
     if is_first_batch:
         load_mi_matrix(x)
         is_first_batch = False
 
-    selected_feature = np.random.choice(2688)  # 随机选择一个特征
+    selected_feature = np.random.choice(2688)
 
     n = round(mask_prob * num_features)
-    # 找到与选定特征互信息值最高的n个特征
     sorted_indices = np.argsort(mi_matrix[selected_feature])
-    mask_indices = sorted_indices[-(n + 1):-1]  # 取互信息值最高的n个特征
+    mask_indices = sorted_indices[-(n + 1):-1]
 
-    # 将选中的特征及其互信息值高的n个特征进行mask
     x_masked = np.copy(x)
     x_masked[:, [selected_feature] + list(mask_indices)] = 0.0
     # np.savetxt('/data/x_masked.txt', x_masked, fmt='%.12f')
-    # 将NumPy数组转换为TensorFlow张量
     x_masked_tensor = tf.convert_to_tensor(x_masked, dtype=tf.float32)
     return x_masked_tensor
 
@@ -133,13 +122,13 @@ def calculate_mutual_information_feat(feat_ids):
         num_samples, num_features = feat_ids.shape
         feat_mi = np.zeros((num_features, num_features))
         for i in range(num_features):
-            for j in range(i + 1, num_features):  # 只计算上三角部分的元素
+            for j in range(i + 1, num_features):
                 x = feat_ids[:, i]
                 y = feat_ids[:, j]
 
                 mi = mutual_info_regression(x.reshape(-1, 1), y)
                 feat_mi[i, j] = mi
-                feat_mi[j, i] = mi  # 填充对称位置的元素
+                feat_mi[j, i] = mi
         with open('/data/mi_matrix_feat_ids.pkl', 'wb') as file:
             pickle.dump(feat_mi, file)
     return feat_mi
@@ -147,7 +136,6 @@ def calculate_mutual_information_feat(feat_ids):
 
 def calculate_frequency_matrix(feat_ids):
     feat_ids = session_run(feat_ids)
-    # 计算feat_ids每一列的值出现的频率
     num_rows, num_cols = feat_ids.shape
     frequency_matrix = np.zeros((num_rows, num_cols))
 
@@ -166,8 +154,8 @@ def cfm_masking_F_F_feat(x, feat_ids, mask_prob):
     x_masked = np.copy(x)
     for i in range(frequency_matrix.shape[0]):
         for j in range(frequency_matrix.shape[1]):
-            mask_probability = frequency_matrix[i, j]  # 获取对应位置的概率值
-            if random.random() < mask_probability * mask_prob:  # 根据概率进行mask
+            mask_probability = frequency_matrix[i, j]
+            if random.random() < mask_probability * mask_prob:
                 x_masked[i, j * 32: (j + 1) * 32] = 0.0
     x_masked_tensor = tf.convert_to_tensor(x_masked, dtype=tf.float32)
     return x_masked_tensor, mask_indices
@@ -176,12 +164,10 @@ def cfm_masking_F_F_feat(x, feat_ids, mask_prob):
 def cfm_masking_F_F_feat_mi(x, feat_ids, mask_prob, num_features):
     x = session_run(x)
     mi_matrix = calculate_mutual_information_feat(feat_ids)
-    selected_feature = np.random.choice(num_features)  # 随机选择一个特征
+    selected_feature = np.random.choice(num_features)
     n = round(mask_prob * num_features)
-    # 找到与选定特征互信息值最高的n个特征
     sorted_indices = np.argsort(mi_matrix[selected_feature])
-    mask_indices = sorted_indices[-(n + 1):-1]  # 取互信息值最高的n个特征
-    # 将选中的特征及其互信息值高的n个特征进行mask
+    mask_indices = sorted_indices[-(n + 1):-1]
     x_masked = np.copy(x)
     for i in mask_indices:
         x_masked[:, range(i * 32, (i + 1) * 32)] = 0.0
@@ -195,25 +181,23 @@ def calculate_mutual_information_feat2(feat_ids):
     num_samples, num_features = feat_ids.shape
     feat_mi = np.zeros((num_features, num_features))
     for i in range(num_features):
-        for j in range(i + 1, num_features):  # 只计算上三角部分的元素
+        for j in range(i + 1, num_features):
             x = feat_ids[:, i]
             y = feat_ids[:, j]
 
             mi = mutual_info_regression(x.reshape(-1, 1), y)
             feat_mi[i, j] = mi
-            feat_mi[j, i] = mi  # 填充对称位置的元素
+            feat_mi[j, i] = mi
     return feat_mi
 
 
 def cfm_masking_F_F_feat_mi2(x, feat_ids, mask_prob, num_features):
     x = session_run(x)
     mi_matrix = calculate_mutual_information_feat2(feat_ids)
-    selected_feature = np.random.choice(num_features)  # 随机选择一个特征
+    selected_feature = np.random.choice(num_features)
     n = round(mask_prob * num_features)
-    # 找到与选定特征互信息值最高的n个特征
     sorted_indices = np.argsort(mi_matrix[selected_feature])
-    mask_indices = sorted_indices[-(n + 1):-1]  # 取互信息值最高的n个特征
-    # 将选中的特征及其互信息值高的n个特征进行mask
+    mask_indices = sorted_indices[-(n + 1):-1]
     x_masked = np.copy(x)
     for i in mask_indices:
         x_masked[:, range(i * 32, (i + 1) * 32)] = 0.0
